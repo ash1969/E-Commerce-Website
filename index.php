b4081e5b256ec4e69b58cfce04a39cddea2b290e
ash1969 <44034455+ash1969@users.noreply.github.com>
2019-01-25 14:02:32 +0530

    Index Page Added

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+
+        <!--jQuery library--> 
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+        <!--Latest compiled and minified JavaScript--> 
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link href="style.css" rel="stylesheet" type="text/css"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Lifestyle Store</title>
+    </head>
+    <body style="padding-top: 50px;">
+        <?php require("includes/common.php"); ?>
+        <?php include("includes/header.php"); ?>
+        <?php
+        if (isset($_SESSION['email'])) 
+            {
+            header('location: products.php');
+            } 
+         ?>
+        
+
+        <div id="content">
+            
+            <div id = "banner_image">
+                <div class="container">	
+                    <center>
+                        <div id="banner_content">
+                            <h1>We sell lifestyle.</h1>
+                            <p>Flat 40% OFF on premium brands </p>
+                            <br/>
+                            <a  href="products.php" class="btn btn-danger btn-lg active">Shop Now</a>
+                        </div>
+                    </center>
+                </div>
+            </div>
+            <!--Main banner image end-->
+
+            <!--Item categories listing-->
+            <div class="container">
+                <div class="row text-center" id="item_list">
+                    <div class="col-sm-4">
+                        <a href="products.php#cameras" >
+                            <div class="thumbnail">
+                                <img src="img/camera.jpg" alt="">
+                                <div class="caption">
+                                    <h3>Cameras</h3>
+                                    <p>Choose among the best available in the world.</p>
+                                </div>
+                            </div> 
+                        </a>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <a href="products.php#watches" >
+                            <div class="thumbnail">
+                                <img src="img/watch.jpg" alt="">
+                                <div class="caption">
+                                    <h3>Watches</h3>
+                                    <p>Original watches from the best brands.</p>
+                                </div>
+                            </div> 
+                        </a>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <a href="products.php#shirts" >
+                            <div class="thumbnail">
+                                <img src="img/shirt.jpg" alt="">
+                                <div class="caption">
+                                    <h3>Shirts</h3>
+                                    <p>Our exquisite collection of shirts.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!--Item categories listing end-->
+        </div>
+
+        <?php include("includes/footer.php"); ?>   
+
+    </body> 
+</html>

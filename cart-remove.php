ba35ea06dc259e01a12b50d54c46fa8e4efb1131
ash1969 <44034455+ash1969@users.noreply.github.com>
2019-06-01 01:12:54 +0530

    Cart Removal Made Functional

--- cart-remove.php
+++ cart-remove.php
@@ -1,8 +1,8 @@
 <?php
 require("includes/common.php");
-$item_id=$_GET['item_id'];
+$item_id=$_GET['id'];
 $user_id=$_SESSION['id'];
-$select_query="DELETE FROM user_items WHERE user_id='$user_id' AND item_id='$item_id'";
+$select_query="DELETE FROM user_items WHERE item_id='$item_id' AND user_id='$user_id'";
 $user_registration_submit = mysqli_query($con, $select_query) or die(mysqli_error($con));
 header('location: cart.php');
 ?>
